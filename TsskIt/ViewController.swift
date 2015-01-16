cfbe159ac210dc3d26cfc6dae8fcecdfd18a2d02
Andy Stannard <andystannawrd@me.com>
2015-01-16 08:10:45 +0000

    Initial Commit

---
+++ TsskIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TsskIt
+//
+//  Created by Andy Stannard on 16/01/2015.
+//  Copyright (c) 2015 inni Accounts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
